45db1a5b032349441486507b96505b3ce03a5e24
danielOkpe <daniel.okpe@epitech.eu>
2025-09-10 18:53:57 +0200

    fix loading notes component

--- secure-notes-front/src/app/pages/dashboard/dashboard.ts
+++ secure-notes-front/src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { Auth } from '../../services/auth';
 import { Router, RouterLink } from '@angular/router';
 import { Navbar } from "../../components/navbar/navbar";
@@ -14,36 +14,42 @@ import { delay } from 'rxjs';
   styleUrl: './dashboard.css'
 })
 export class Dashboard implements OnInit {
-  private auth = inject(Auth);
-  private router = inject(Router);
   private noteService = inject(Notes);
   notes : Note[] = [];
+  isLoading = true;
   userId : number = +localStorage.getItem("userId")!;
+  private cdr = inject(ChangeDetectorRef);
 
   ngOnInit(): void {
-
+    this.isLoading = true;
     this.noteService.getNotesByUser(this.userId)
      .pipe(
       delay(500) // délai en millisecondes, ici 2 secondes
     )
     .subscribe({
        next: (res) => {
-       this.notes = res.sort((a, b) => {
-          const dateA = new Date(a.updated_at || a.created_at);
-          const dateB = new Date(b.updated_at || b.created_at);
-          return dateB.getTime() - dateA.getTime(); 
-        });
-       console.log('my notes', this.notes)
+          if (res && Array.isArray(res)) {
+            this.notes = res.sort((a, b) => {
+              const dateA = new Date(a.updated_at || a.created_at);
+              const dateB = new Date(b.updated_at || b.created_at);
+              return dateB.getTime() - dateA.getTime();
+            });
+            console.log('Sorted notes:', this.notes);
+            console.log('Final notes length:', this.notes.length);
+          } else {
+            console.error('Response is not an array:', res);
+            this.notes = [];
+          }
+       this.isLoading = false;
+       console.log('my notes', this.notes);
+       this.cdr.detectChanges();
       },
       error: (error) => {
+        this.isLoading = false;
         console.error('Error when get user notes', error)
       }
     });
     
   }
 
-  logout(){
-    this.auth.logout().subscribe()
-    this.router.navigate(['/']);
-  }
 }
